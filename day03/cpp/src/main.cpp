05413fed1fc667e5185a2f2e14de00be9f75bcdf
Butch Howard <butchhoward@gmail.com>
2018-12-03 08:38:26 -0500

    WIP

--- day03/cpp/src/main.cpp
+++ day03/cpp/src/main.cpp
@@ -6,6 +6,10 @@
 #include <tuple>
 #include <vector>
 
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
 std::vector<std::string> read_file(const std::string& filename)
 {
     std::vector<std::string> DataArray;
@@ -24,6 +28,23 @@ std::vector<std::string> read_file(const std::string& filename)
     }
 
     return DataArray;
+}
+
+void parse_input(const std::string& value)
+{
+    //#1 @ 1,3: 4x4
+    std::istringstream stream(value);
+    int n;
+    double f;
+    bool b;
+ 
+    stream >> n >> f >> std::boolalpha >> b;
+    std::cout << "n = " << n << '\n'
+              << "f = " << f << '\n'
+              << "b = " << std::boolalpha << b << '\n';
+ 
+    // extract the rest using the streambuf overload
+    stream >> std::cout.rdbuf();
 }
 
 int main(int argc, char *argv[])
